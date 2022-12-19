d8a3bcc3cb9b2dfbfbcbbdefabc7c9e52e013f28
CommunityBrands_OEA <119939725+Sarang-CommunityBrands@users.noreply.github.com>
2022-12-19 21:30:57 +0530

    Update InfraOEASet.ps1
    changes

--- InfraOEASet.ps1
+++ InfraOEASet.ps1
@@ -38,13 +38,11 @@ Set-AzKeyVaultSecret -VaultName $keyVaultName -Name $secretRelyingPartySecretNam
 
 
 #setting the github account
-$config = New-AzSynapseGitRepositoryConfig -RepositoryType GitHub -AccountName Sarang-CommunityBrands -RepositoryName OEA -CollaborationBranch dev-main
-$password = ConvertTo-SecureString "Communitybrands@123" -AsPlainText -Force
-$creds = New-Object System.Management.Automation.PSCredential ("Sarang.Kulkarni@communitybrands.com", $password)
-Update-AzSynapseWorkspace -ResourceGroupName cbuk-oea-dev-rg -Name $synapseName -GitRepository $config
+$config = New-AzSynapseGitRepositoryConfig -RepositoryType GitHub -AccountName Sarang-CommunityBrands -RepositoryName OEA -CollaborationBranch main
+Update-AzSynapseWorkspace -ResourceGroupName $resource_group -Name $synapseName -GitRepository $config
 
 }
 
 # function call
-az login
+#az login
 Set-OEA 79a9a1b8-17c9-4098-bbae-16c21b7edd2a cbuk-oea-dev-rg cbuk-oea-dev-kv cbuk-oea-dev-syn
